2b8d9f8a9cd8914302250cbdeab3f4a3adfc030c
houleymatou-diallo <dhouleymatou150@gmail.com>
2025-07-10 20:18:49 +0000

    initialisation du projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentListComponent } from './features/students/components/student-list/student-list.component';
+import { LoginComponent } from './features/auth/components/login/login.component';
+import { DashboardHomeComponent } from './features/dashboard/components/dashboard-home/dashboard-home.component';
+import { TeacherListComponent } from './features/teachers/components/teacher-list/teacher-list.component';
+import { SubjectListComponent } from './features/subjects/components/subject-list/subject-list.component';
+import { GradeEntryComponent } from './features/grades/components/grade-entry/grade-entry.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentListComponent,
+    LoginComponent,
+    DashboardHomeComponent,
+    TeacherListComponent,
+    SubjectListComponent,
+    GradeEntryComponent
   ],
   imports: [
     BrowserModule,
