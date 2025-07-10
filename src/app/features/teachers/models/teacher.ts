51fccb05ad8840d66b027da6bd536bff1dece49c
houleymatou-diallo <dhouleymatou150@gmail.com>
2025-07-10 20:20:53 +0000

    creation de l'interface teacher.ts

---
+++ src/app/features/teachers/models/teacher.ts
@@ -0,0 +1,11 @@
+export interface Teacher {
+  id: number;
+  userId: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone?: string;
+  hireDate: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
