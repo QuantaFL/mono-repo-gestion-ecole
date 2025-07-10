936bd51fd37ffdaf06cce5cc321cd57695a32ec0
houleymatou-diallo <dhouleymatou150@gmail.com>
2025-07-10 22:07:02 +0000

    ajout du component dashboard et visualisation du ui et ux

--- src/app/features/auth/components/login/login.component.ts
+++ src/app/features/auth/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Router, RouterModule} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private router:Router) {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Formulaire soumis', this.loginForm.value);
+      this.router.navigate(['/dashboard']);
+    } else {
+      console.log('Formulaire invalide');
+    }
+  }
 }
