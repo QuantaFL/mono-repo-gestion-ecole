100a40f0b64cfc70b6fc0eecb345d174caf7d102
houleymatou-diallo <dhouleymatou150@gmail.com>
2025-07-10 23:44:31 +0000

    ajout  des routes pour le feature de classe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,20 @@ import {LoginComponent} from "./features/auth/components/login/login.component";
 import {DashboardHomeComponent} from "./features/dashboard/components/dashboard-home/dashboard-home.component";
 import {StudentListComponent} from "./features/students/components/student-list/student-list.component";
 import {AddStudentComponent} from "./features/students/components/add-student/add-student.component";
+import {TeacherListComponent} from "./features/teachers/components/teacher-list/teacher-list.component";
+import {AddTeacherComponent} from "./features/teachers/components/add-teacher/add-teacher.component";
+import {ClassAddComponent} from "./features/class/components/class-add/class-add.component";
+import {ClassListComponent} from "./features/class/components/class-list/class-list.component";
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'dashboard', component: DashboardHomeComponent },
   { path: 'list_student', component: StudentListComponent },
   { path: 'student', component: AddStudentComponent },
+  { path: 'teacher', component: AddTeacherComponent },
+  { path: 'list_teacher', component: TeacherListComponent},
+  { path: 'list_class', component: ClassAddComponent},
+  { path: 'class', component: ClassListComponent},
 ];
 
 @NgModule({
