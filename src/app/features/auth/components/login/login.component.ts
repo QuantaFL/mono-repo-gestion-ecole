f90bf769eb6f49b0c5cc4273bcf6b9fbb84e26bf
houleymatou-diallo <dhouleymatou150@gmail.com>
2025-07-12 01:52:13 +0000

    feat(login): integrate auth service

--- src/app/features/auth/components/login/login.component.ts
+++ src/app/features/auth/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {Router, RouterModule} from "@angular/router";
+import {AuthService} from "../../services/auth.service";
+import {LoginRequest} from "../../models/loginRequest";
 
 @Component({
   selector: 'app-login',
@@ -9,8 +11,9 @@ import {Router, RouterModule} from "@angular/router";
 })
 export class LoginComponent {
   loginForm: FormGroup;
+  errorMessage = "";
 
-  constructor(private router:Router) {
+  constructor(private router:Router,private authService:AuthService) {
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', Validators.required),
@@ -20,7 +23,22 @@ export class LoginComponent {
   onSubmit() {
     if (this.loginForm.valid) {
       console.log('Formulaire soumis', this.loginForm.value);
-      this.router.navigate(['/dashboard']);
+        const { email, password } = this.loginForm.value;
+        const loginRequest:LoginRequest = {
+          email,
+          password,
+        }
+      //   this.authService.login(loginRequest).subscribe({
+      //     next: (response: any) => {
+      //       localStorage.setItem('token', response.token);
+      //       console.log(localStorage.getItem("token"));// stockage du token
+      //       this.router.navigate(['/dashboard']);
+      //     },
+      //     error: (err) => {
+      //       this.errorMessage = 'Identifiants incorrects';
+      //     }
+      //   });
+       this.router.navigate(['/dashboard']);
     } else {
       console.log('Formulaire invalide');
     }
