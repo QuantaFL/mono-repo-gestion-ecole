9b0b081fc780116966cf0447cfe03f6b224746e1
houleymatou-diallo <dhouleymatou150@gmail.com>
2025-07-12 01:52:22 +0000

    feat(auth): implement login method in AuthService

--- src/app/features/auth/services/auth.service.ts
+++ src/app/features/auth/services/auth.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
-
+import {HttpClient} from "@angular/common/http";
+import {User} from "../models/user";
+import {LoginRequest} from "../models/loginRequest";
+import {Observable} from "rxjs";
+import {LoginResponse} from "../models/LoginResponse";
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  private baseUrl = 'http://localhost:8000';
+  login(data: LoginRequest): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(`${this.baseUrl}/login`, data);
+  }
 }
