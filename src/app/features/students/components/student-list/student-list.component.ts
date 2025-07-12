a95b6e0b02b13ab0d70ab49977169a83d2dceefe
houleymatou-diallo <dhouleymatou150@gmail.com>
2025-07-12 15:52:21 +0000

    Refactor:Affichage de la liste des students en utilisant les donnees moquees

--- src/app/features/students/components/student-list/student-list.component.ts
+++ src/app/features/students/components/student-list/student-list.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {StudentService} from "../../services/student.service";
+import {Router} from "@angular/router";
+import {Student} from "../../models/student";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-student-list',
@@ -6,5 +10,10 @@ import { Component } from '@angular/core';
   styleUrl: './student-list.component.scss'
 })
 export class StudentListComponent {
-
+constructor(private studentService: StudentService,router:Router) {
+}
+students:Student[] = [];
+ngOnInit() {
+  this.studentService.getAllStudents().subscribe(data=>this.students=data);
+}
 }
