e5ee47b3359275bf91dcd84b07d8c3c30a4ede1d
houleymatou-diallo <dhouleymatou150@gmail.com>
2025-07-12 16:04:10 +0000

    Refactor:Correction du ficier ts de class

--- src/app/features/class/services/class.service.ts
+++ src/app/features/class/services/class.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { Class } from '../models/class';
+import { ClassModel } from '../models/class';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClassService {
-  private classes: Class[] = [
-    { id: 1, name: 'Math 101', teacherId: 1 },
-    { id: 2, name: 'Science 101', teacherId: 2 },
-    { id: 3, name: 'History 101', teacherId: 3 }
+  private classes: ClassModel[] = [
+    { id: 1, name: 'Math 101', academicYear: '2024-2025' },
+    { id: 2, name: 'Science 101', academicYear: '2024-2025' },
+    { id: 3, name: 'History 101', academicYear: '2024-2025' }
   ];
+
 
   constructor() { }
 
-  getAll(): Observable<Class[]> {
+  getAll(): Observable<ClassModel[]> {
     return of(this.classes);
   }
 }
