977db30ae2600f13c512709208f6c13ede3ebebe
houleymatou-diallo <dhouleymatou150@gmail.com>
2025-07-15 12:59:31 +0000

    Refactor: Update class-add component logic

--- src/app/features/class/components/class-add/class-add.component.ts
+++ src/app/features/class/components/class-add/class-add.component.ts
@@ -1,23 +1,41 @@
 import { Component } from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { ClassService } from '../../services/class.service';
 
 @Component({
   selector: 'app-class-add',
   templateUrl: './class-add.component.html',
-  styleUrl: './class-add.component.scss'
+  styleUrls: ['./class-add.component.scss']
 })
 export class ClassAddComponent {
   classForm = new FormGroup({
     name: new FormControl('', Validators.required),
-    academicYear: new FormControl('', Validators.required)
+    academic_year: new FormControl('', Validators.required)
   });
 
+  success: string | null = null;
+  error: string | null = null;
+
+  constructor(private classService: ClassService) {}
+
   onSubmit(): void {
+    this.success = null;
+    this.error = null;
     if (this.classForm.valid) {
       const newClass = this.classForm.value;
-      console.log('Classe ajoutée :', newClass);
-      // À connecter plus tard avec le service
-      this.classForm.reset();
+      console.log(newClass);
+      this.classService.create({
+        name: newClass.name!,
+        academicYear: newClass.academic_year!
+      }).subscribe({
+        next: (res) => {
+          this.success = 'Classe ajoutée avec succès';
+          this.classForm.reset();
+        },
+        error: (err) => {
+          this.error = 'Erreur lors de l\'ajout de la classe';
+        }
+      });
     }
   }
 }
