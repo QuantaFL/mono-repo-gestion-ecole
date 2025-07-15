a8bc9ae2f5a124178f3954f196e2e103963e58f2
houleymatou-diallo <dhouleymatou150@gmail.com>
2025-07-15 12:59:38 +0000

    Refactor: Update class-list component logic

--- src/app/features/class/components/class-list/class-list.component.ts
+++ src/app/features/class/components/class-list/class-list.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClassService } from '../../services/class.service';
+import { ClassModel } from '../../models/class';
 
 @Component({
   selector: 'app-class-list',
   templateUrl: './class-list.component.html',
-  styleUrl: './class-list.component.scss'
+  styleUrls: ['./class-list.component.scss']
 })
-export class ClassListComponent {
+export class ClassListComponent implements OnInit {
+  classes: ClassModel[] = [];
+  loading = false;
+  error: string | null = null;
 
+  constructor(private classService: ClassService) {}
+
+  ngOnInit(): void {
+    this.fetchClasses();
+  }
+
+  fetchClasses(): void {
+    this.loading = true;
+    this.classService.getAll().subscribe({
+      next: (res) => {
+        this.classes = res.classes || [];
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = 'Erreur lors du chargement des classes';
+        this.loading = false;
+      }
+    });
+  }
+
+  deleteClass(id: number): void {
+    if (!confirm('Voulez-vous vraiment supprimer cette classe ?')) return;
+    this.classService.delete(id).subscribe({
+      next: () => {
+        this.classes = this.classes.filter(c => c.id !== id);
+      },
+      error: () => {
+        this.error = 'Erreur lors de la suppression';
+      }
+    });
+  }
 }
