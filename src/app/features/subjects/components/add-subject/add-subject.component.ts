b53a711ab3e78ed904192b1ffddd1e2ea280f2e5
houleymatou-diallo <dhouleymatou150@gmail.com>
2025-07-22 16:11:02 +0000

    Feat: add add-subject component

---
+++ src/app/features/subjects/components/add-subject/add-subject.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SubjectService } from '../../services/subject.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-add-subject',
+  templateUrl: './add-subject.component.html',
+  styleUrls: ['./add-subject.component.scss']
+})
+export class AddSubjectComponent {
+  subjectForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private subjectService: SubjectService,
+    private router: Router
+  ) {
+    this.subjectForm = this.fb.group({
+      name: ['', Validators.required],
+      coefficient: ['', [Validators.required, Validators.min(1)]],
+      level: ['']
+    });
+  }
+
+  onSubmit(): void {
+    if (this.subjectForm.valid) {
+        const subjectData = this.subjectForm.value;
+
+
+    }
+  }
+}
