d400399e6cfdc329861180a38bf6ce72d686f391
houleymatou-diallo <dhouleymatou150@gmail.com>
2025-07-22 16:09:08 +0000

    Feat: fetch and display subjects

--- src/app/features/subjects/components/subject-list/subject-list.component.ts
+++ src/app/features/subjects/components/subject-list/subject-list.component.ts
@@ -1,9 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SubjectService } from '../../services/subject.service';
+import { Subject } from '../../models/subject';
 
 @Component({
   selector: 'app-subject-list',
   templateUrl: './subject-list.component.html',
   styleUrl: './subject-list.component.scss'
 })
-export class SubjectListComponent {
+export class SubjectListComponent implements OnInit {
+  subjects: Subject[] = [];
+  loading = false;
+  error: string | null = null;
+
+  constructor(private subjectService: SubjectService) {}
+
+  ngOnInit(): void {
+    this.fetchSubjects();
+  }
+
+  fetchSubjects(): void {
+    this.loading = true;
+    this.subjectService.getAllSubject().subscribe({
+      next: (res) => {
+        console.log(res);
+        // Flatten nested array if needed
+        if (Array.isArray(res) && Array.isArray(res[0])) {
+          this.subjects = res[0];
+        } else if (Array.isArray(res)) {
+          this.subjects = res;
+        } else if (res) {
+          this.subjects = [res];
+        } else {
+          this.subjects = [];
+        }
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = 'Erreur lors du chargement des matières';
+        this.loading = false;
+      }
+    });
+  }
 }
