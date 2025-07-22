bf37a8a2472ff8bfe95f9f4e322b9d403051e4f9
houleymatou-diallo <dhouleymatou150@gmail.com>
2025-07-22 17:18:32 +0000

    Refactor: update teacher list component to fetch teachers from service and handle loading state

--- src/app/features/teachers/components/teacher-list/teacher-list.component.ts
+++ src/app/features/teachers/components/teacher-list/teacher-list.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Teacher} from "../../models/teacher";
+import {TeacherService} from "../../services/teacher.service";
 
 @Component({
   selector: 'app-teacher-list',
   templateUrl: './teacher-list.component.html',
   styleUrl: './teacher-list.component.scss'
 })
-export class TeacherListComponent {
- teachers:Teacher[] = [];
+export class TeacherListComponent implements OnInit {
+  teachers: Teacher[] = [];
+  loading = false;
+  error: string | null = null;
+
+  constructor(private teacherService: TeacherService) {}
+
+  ngOnInit(): void {
+    this.fetchTeachers();
+  }
+
+  fetchTeachers(): void {
+    this.loading = true;
+    this.teacherService.getAllTeacher().subscribe({
+      next: (res) => {
+        console.log(res);
+        this.teachers = res;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = 'Erreur lors du chargement des enseignants';
+        this.loading = false;
+      }
+    });
+  }
 }
