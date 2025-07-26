a806b7fb785a28295d7a99fd67eeff4db5c0a132
traorecheikh <atidiane741@gmail.com>
2025-07-26 17:11:19 +0000

    Feat: update login flow with role-based navigation and extend LoginResponse interface

--- src/app/features/auth/components/login/login.component.ts
+++ src/app/features/auth/components/login/login.component.ts
@@ -3,6 +3,7 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {Router, RouterModule} from "@angular/router";
 import {AuthService} from "../../services/auth.service";
 import {LoginRequest} from "../../requests/loginRequest";
+import {LoginResponse} from "../../requests/LoginResponse";
 
 @Component({
   selector: 'app-login',
@@ -22,22 +23,34 @@ export class LoginComponent {
 
   onSubmit() {
     if (this.loginForm.valid) {
-      const { email, password } = this.loginForm.value;
-      const loginRequest: LoginRequest = { email, password };
-      this.authService.login(loginRequest).subscribe({
-        next: (response: any) => {
-          localStorage.setItem('token', response.token);
-          localStorage.setItem('user', JSON.stringify(response.user));
-          if (response.user.isFirstLogin) {
-            this.router.navigate(['/change-password']);
-          } else {
-            this.router.navigate(['/dashboard']);
-          }
-        },
-        error: (err) => {
-          this.errorMessage = 'Identifiants incorrects';
+      console.log('Formulaire soumis', this.loginForm.value);
+        const { email, password } = this.loginForm.value;
+        const loginRequest:LoginRequest = {
+          email,
+          password,
         }
-      });
+      this.authService.login(loginRequest).subscribe({
+          next: async (response: LoginResponse) => {
+            localStorage.setItem('token', response.token);
+            localStorage.setItem('user', JSON.stringify(response.user));
+            console.log(localStorage.getItem("token"));
+            if(response.user.role.name === 'teacher') {
+              await this.router.navigate(['/teacher-dashboard']);
+            } else if(response.user.role.name === 'admin') {
+            await this.router.navigate(['/dashboard']);
+            }else{
+              console.log(response.user)
+              console.log('Vous netes pas autorisé à accéder à cette application')
+              this.errorMessage = 'Vous netes pas autorisé à accéder à cette application';
+              //TODO navigate to unauthorized page as its either a student or parent
+              localStorage.removeItem('token');
+              localStorage.removeItem('user');
+            }
+          },
+          error: (err) => {
+            this.errorMessage = 'Identifiants incorrects';
+          }
+        });
     } else {
       console.log('Formulaire invalide');
     }
--- src/app/features/auth/requests/LoginResponse.ts
+++ src/app/features/auth/requests/LoginResponse.ts
@@ -1,11 +1,20 @@
 export interface LoginResponse {
-  token: string;
   user: {
     id: number;
-    name: string;
+    first_name: string;
+    last_name: string;
+    birthday: string;
     email: string;
+    adress: string;
+    phone: string;
     role_id: number;
+    role: {
+      id: number;
+      name: string;
+    };
+    created_at: string;
+    updated_at: string;
+    isFirstLogin: boolean;
   };
-  message: string;
-  success: boolean;
+  token: string;
 }
