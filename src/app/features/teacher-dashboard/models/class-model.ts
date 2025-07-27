e9ed35fc3596f981e881dbd39ec8c75869384f8c
RBen19 <bonmerciel@gmail.com>
2025-07-27 00:15:54 +0000

    feat: make id fiel nullable

--- src/app/features/teacher-dashboard/models/class-model.ts
+++ src/app/features/teacher-dashboard/models/class-model.ts
@@ -1,7 +1,7 @@
 import {StudentSession} from "./student-session";
 
 export interface ClassModel {
-  id: number;
+  id?: number;
   name: string;
   level: string;
   latest_student_session:StudentSession[];
