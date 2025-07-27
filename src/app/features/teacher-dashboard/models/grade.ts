01b070c4ab0922e763158b1ce4bb055fe7400c98
traorecheikh <atidiane741@gmail.com>
2025-07-27 17:13:56 +0000

    Feat: make Grade interface properties optional for improved flexibility

--- src/app/features/teacher-dashboard/models/grade.ts
+++ src/app/features/teacher-dashboard/models/grade.ts
@@ -3,15 +3,15 @@ import { Assignment } from './assignment';
 import { Term } from './term';
 
 export interface Grade {
-  id: number;
+  id?: number;
   mark: number | null;
   type: string | null;
-  created_at: string;
-  updated_at: string;
+  created_at?: string;
+  updated_at?: string;
   assignement_id: number;
   student_session_id: number;
   term_id: number;
-  assignement: Assignment;
-  student_session: StudentSession;
-  term: Term;
+  assignement?: Assignment;
+  student_session?: StudentSession;
+  term?: Term;
 }
