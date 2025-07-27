ba118c3e61dc47a36b23350c161bf2edd757fad0
traorecheikh <atidiane741@gmail.com>
2025-07-27 17:13:35 +0000

    Feat: add teacher my classes component with class fetching and navigation functionality

---
+++ src/app/features/teacher-dashboard/components/teacher-my-classes/teacher-my-classes.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TeacherDashboardService } from '../../services/teacher-dashboard.service';
+import { TeacherStore } from '../../services/teacher.store';
+import { ClassModel } from '../../models/class-model';
+
+@Component({
+  selector: 'app-teacher-my-classes',
+  templateUrl: './teacher-my-classes.component.html',
+  styleUrls: ['./teacher-my-classes.component.scss']
+})
+export class TeacherMyClassesComponent implements OnInit {
+  classes: ClassModel[] = [];
+  loading = true;
+
+  constructor(
+    private teacherDashboardService: TeacherDashboardService,
+    private teacherStore: TeacherStore,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.teacherStore.currentTeacher$.subscribe(teacher => {
+      if (teacher) {
+        this.teacherDashboardService.getCurrentAcademicYear().subscribe({
+          next: (year) => {
+            this.teacherDashboardService.getTeacherClasses(teacher.id, year.id)
+              .subscribe({
+                next: (classes) => {
+                  this.classes = classes;
+                  this.loading = false;
+                },
+                error: () => {
+                  this.classes = [];
+                  this.loading = false;
+                }
+              });
+          },
+          error: () => {
+            this.loading = false;
+          }
+        });
+      } else {
+        this.loading = false;
+      }
+    });
+  }
+
+  goToClassDetails(classId: number): void {
+    this.router.navigate(['/teacher-dashboard/class-details', classId]);
+  }
+}
