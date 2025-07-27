b754342aefe55958c58050f11c04dcc78b6c6f23
RBen19 <bonmerciel@gmail.com>
2025-07-27 01:36:15 +0000

    feat: declare adn implement go to class

--- src/app/features/class/components/class-list/class-list.component.ts
+++ src/app/features/class/components/class-list/class-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ClassService } from '../../services/class.service';
 import { ClassModel } from '../../../teacher-dashboard/models/class-model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-class-list',
@@ -8,43 +9,28 @@ import { ClassModel } from '../../../teacher-dashboard/models/class-model';
   styleUrls: ['./class-list.component.scss']
 })
 export class ClassListComponent implements OnInit {
- /*
-   classes: any[] = [
-    {
-      id: 1,
-      name: '6e',
-      level: 'Collège',
-      latest_student_session: [
-        { id: 1, student_id: 1, student_name: 'Alice', grade: 15 },
-        { id: 2, student_id: 2, student_name: 'Bob', grade: null },
-        { id: 3, student_id: 3, student_name: 'Charlie', grade: 12 }
-      ]
-    },
-    {
-      id: 2,
-      name: '5e',
-      level: 'Collège',
-      latest_student_session: [
-        { id: 4, student_id: 4, student_name: 'David', grade: 14 },
-        { id: 5, student_id: 5, student_name: 'Eva', grade: null }
-      ]
-    },
-    {
-      id: 3,
-      name: '4e',
-      level: 'Collège',
-      latest_student_session: []
-    }
-    // Add more mocked classes as needed
-  ];
- */
+  
+  constructor(
+    private classService: ClassService,
+    private router: Router
+  ) {
+    
+  }
+goToClass() {
+   this.router.navigateByUrl('/class'); 
+}
+  addSubject(_t21: ClassModel) {
+  throw new Error('Method not implemented.');
+  }
+  generateBulletin(_t21: ClassModel) {
+  throw new Error('Method not implemented.');
+  }
+
    classes: ClassModel[] = [];
   loading = false;
   error: string | null = null;
 
-  constructor(
-    private classService: ClassService
-  ) {}
+ 
 
   ngOnInit(): void {
         this.getAllClasses();
