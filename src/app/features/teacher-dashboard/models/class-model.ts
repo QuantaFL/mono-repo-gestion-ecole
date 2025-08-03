74acd861389cd184469e06d8001b6dfba9e30978
RBen19 <bonmerciel@gmail.com>
2025-08-03 12:48:21 +0000

    feat(subject model on teacher): add subject in class model

--- src/app/features/teacher-dashboard/models/class-model.ts
+++ src/app/features/teacher-dashboard/models/class-model.ts
@@ -1,11 +1,30 @@
 import {StudentSession} from "./student-session";
 
+export interface SubjectPivot {
+  class_model_id: number;
+  subject_id: number;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface ClassSubject {
+  id: number;
+  name: string;
+  level: string;
+  coefficient: number;
+  status: boolean;
+  created_at: string;
+  updated_at: string;
+  pivot?: SubjectPivot;
+}
+
 export interface ClassModel {
   id?: number;
   name: string;
   level: string;
   latest_student_session: StudentSession | null;
   current_academic_year_student_sessions: StudentSession[];
+  subjects?: ClassSubject[];
   created_at?: string;
   updated_at?: string;
 }
