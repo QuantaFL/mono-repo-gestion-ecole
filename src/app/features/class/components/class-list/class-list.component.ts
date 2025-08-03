3a73c39a330785c87a5e80aec13a31621e55eab3
RBen19 <bonmerciel@gmail.com>
2025-08-03 10:50:57 +0000

    feat: update view

--- src/app/features/class/components/class-list/class-list.component.ts
+++ src/app/features/class/components/class-list/class-list.component.ts
@@ -16,19 +16,53 @@ export class ClassListComponent implements OnInit {
   ) {
     
   }
-goToClass() {
-   this.router.navigateByUrl('/class'); 
-}
-  addSubject(_t21: ClassModel) {
-  throw new Error('Method not implemented.');
-  }
-  generateBulletin(_t21: ClassModel) {
-  throw new Error('Method not implemented.');
-  }
-
-   classes: ClassModel[] = [];
+  
+  classes: ClassModel[] = [];
   loading = false;
   error: string | null = null;
+  
+  // Modal properties
+  isModalOpen = false;
+  isViewSubjectsModalOpen = false;
+  selectedClass: ClassModel | null = null;
+
+  goToClass() {
+    this.router.navigateByUrl('/class'); 
+  }
+  
+  addSubject(classe: ClassModel) {
+    console.log('Opening modal for class:', classe);
+    this.selectedClass = classe;
+    this.isModalOpen = true;
+  }
+  
+  generateBulletin(_t21: ClassModel) {
+    throw new Error('Method not implemented.');
+  }
+  
+  viewSubjects(classe: ClassModel) {
+    console.log('Opening subjects view for class:', classe);
+    this.selectedClass = classe;
+    this.isViewSubjectsModalOpen = true;
+  }
+
+  closeModal() {
+    console.log('Closing modal');
+    this.isModalOpen = false;
+    this.selectedClass = null;
+  }
+  
+  closeViewSubjectsModal() {
+    console.log('Closing view subjects modal');
+    this.isViewSubjectsModalOpen = false;
+    this.selectedClass = null;
+  }
+  
+  onSubjectAssigned(response: any) {
+    console.log('Subject assigned successfully:', response);
+    // Optionally refresh the class list or show a success message
+    this.getAllClasses();
+  }
 
  
 
@@ -43,6 +77,8 @@ goToClass() {
     getAllClasses(): void {
     this.classService.getAll().subscribe({
       next: (classesResponse) => {
+        console.log('Classes fetched successfully:',classesResponse);
+        this.classes = classesResponse.reverse(); // Reverse to show the latest classes first
         this.classes = classesResponse;
       },
       error: () => {
